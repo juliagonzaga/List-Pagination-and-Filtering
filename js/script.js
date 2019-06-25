b778bf57520428dc579e6d7f6532972291f92b16
Julia Emily Gonzaga <shaow.1230@gmail.com>
2019-06-25 23:36:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
+document.addEventListener('DOMContentLoaded', () => {
 /*** 
    Add your global variables that store the DOM elements you will 
    need to reference and/or manipulate. 
@@ -16,7 +16,8 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-
+var studentsList = document.querySelectorAll('.student-item');
+var studentsPerPage = 10;
 
 
 
@@ -34,7 +35,19 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
+const showPage = (list,page) => {
+   const startIndex = (page * studentsPerPage) - studentsPerPage;
+   const endIndex = page * studentsPerPage;
+   for (let i = 0 ; i < list.length; i++){
+      if(i >= startIndex && i < endIndex){
+         list[i].style.display = 'block';
+      } else {
+         list[i].style.display = 'none';
+      }
+   }
+};
 
+showPage(studentsList, 1);
 
 
 
@@ -42,9 +55,34 @@ FSJS project 2 - List Filter and Pagination
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+const appendPageLinks = (list) => {
+   const pages = Math.ceil(list.length/studentsPerPage);
+   const mainDiv = document.querySelector('.page');
+   const div = document.createElement('div');
+   const ul = document.createElement('ul');
+   div.className = 'pagination';
+   mainDiv.appendChild(div);
+   div.appendChild(ul);
 
+   for(let i = 0; i < pages; i++ ){
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      if(i === 0){
+         a.className = 'active';
+      }
+      a.href = '#';
+      ul.appendChild(li);
+      li.appendChild(a);
+      a.text = i + 1;
+      a.addEventListener = ('click', () => {
+         
+      })
 
+   }
+};
+appendPageLinks(studentsList);
 
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
+});
